d67cb80eb8a850fbb64c3075f265c21af150a2c8
Mykola <mykola.novytskyi@gmail.com>
2024-05-21 23:01:23 +0300

    refactor(api) remove project api and remove unused nest.js packages

--- apps/api/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Message } from '@marsh-charts/api-interfaces';
-
-@Injectable()
-export class AppService {
-  getData(): Message {
-    return { message: 'Welcome to api!' };
-  }
-}
